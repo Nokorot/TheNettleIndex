e28da33efc2ea1722ad437314a327308066207a1
Nokorot <torhaakon2012@hotmail.com>
2025-06-05 15:37:31 +0200

    Script for transing Imgur, instead of at html request

---
+++ scripts/imgur_images.py
@@ -0,0 +1,102 @@
+import os
+import sys
+from typing import Optional
+
+import pyimgur
+import pymongo
+from pymongo.auth import ConfigurationError
+
+sys.path.append(os.path.curdir)
+
+from src.logging import LoggerContext
+
+logger = LoggerContext("")
+
+ICON_FOLDER = "./media/entry_icons"
+
+
+def read_toml(fname):
+    import pytoml
+
+    with open(fname, "rb") as fin:
+        try:
+            return pytoml.load(fin)
+        except Exception as e:
+            print("Toml Error: ", e)
+            exit(1)
+
+
+app_secrets = read_toml(".secrets.toml")
+
+imgur_secrets: Optional[dict] = app_secrets.get("Imgur")
+assert imgur_secrets is not None, "Failed to find Imgur secrets"
+
+imgur = pyimgur.Imgur(
+    imgur_secrets["CLIENT_ID"],
+    imgur_secrets["CLIENT_SECRET"],
+    access_token=imgur_secrets["ACCESS_TOKEN"],
+    refresh_token=imgur_secrets["REFRESH_TOKEN"],
+)
+
+
+def imgur_upload_image(str_id: str, title: str) -> Optional[str]:
+    logger.INFO(f"Uploading image for {str_id}")
+
+    image_path = os.path.join(ICON_FOLDER, f"{str_id}.img")
+    if not os.path.exists(image_path):
+        logger.INFO(f"Image path {image_path} does not exist")
+
+        return None
+
+    imgur_image = imgur.upload_image(image_path, title=title)
+    if imgur_image is None:
+        logger.INFO("Failed to upload image")
+        return None
+
+    image_url = imgur_image.link
+    logger.INFO(f"Image '{image_path}' uploaded to '{image_url}'")
+
+    return image_url
+
+
+mdb_secrets: Optional[dict] = app_secrets.get("MongoDB")
+if mdb_secrets is None:
+    logger.ERROR("Missing MongoDB secrets")
+    exit(1)
+uri: str = mdb_secrets.get("URI")
+db_name = mdb_secrets.get("db_name")
+
+try:
+    client = pymongo.MongoClient(uri)
+    mongodb = client[db_name]
+    logger.INFO("MongoDB connected")
+except ConfigurationError:
+    logger.ERROR(
+        "An Invalid URI host error was received.\n"
+        "Is your Atlas host name correct in your connection string?"
+    )
+    exit(1)
+
+entries_coln = mongodb["entries"]
+
+DB_ENTRY_VERSION = "0.2.0"
+
+qfilter = {}
+
+for c in entries_coln.find(qfilter):
+    _id = c.get("_id")
+    str_id = str(_id)
+
+    version = c.get("version")
+
+    print(_id, version)
+
+    if version is None:
+
+        new_url = imgur_upload_image(str_id, c.get("name") or "")
+
+        if new_url is not None:
+            entries_coln.update_one(
+                {"_id": _id},
+                {"$set": {"icon_url": new_url, "version": DB_ENTRY_VERSION}},
+            )
--- src/web/main.py
+++ src/web/main.py
@@ -29,27 +29,7 @@ def route(app: NettleApp):
         sys.exit(1)
 
     FOLDERS: dict = app.config["FOLDERS"]
-    ICON_FOLDER = FOLDERS["ICON"]
     UPLOAD_FOLDER = FOLDERS["UPLOAD"]
-
-    def imgur_upload_image(str_id: str, title: str) -> Optional[str]:
-        logger.INFO(f"Uploading image for {str_id}")
-
-        image_path = os.path.join(ICON_FOLDER, f"{str_id}.img")
-        if not os.path.exists(image_path):
-            logger.INFO(f"Image path {image_path} does not exist")
-
-            return None
-
-        imgur_image = app.imgur.upload_image(image_path, title=title)
-        if imgur_image is None:
-            logger.INFO("Failed to upload image")
-            return None
-
-        image_url = imgur_image.link
-        logger.INFO(f"Image '{image_path}' uploaded to '{image_url}'")
-
-        return image_url
 
     @flask_app.route("/")
     def home():
@@ -61,17 +41,7 @@ def route(app: NettleApp):
             str_id = str(_id)
 
             version = c.get("version")
-
-            if version is None:
-                new_url = imgur_upload_image(str_id, c.get("name") or "")
-
-                if new_url is not None:
-                    coln.update_one(
-                        {"_id": _id},
-                        {"$set": {"icon_url": new_url, "version": DB_ENTRY_VERSION}},
-                    )
-
-            image_url = c.get("icon_url")
+            _ = version
 
             entries.append(
                 {
@@ -79,45 +49,12 @@ def route(app: NettleApp):
                     "name": c.get("name"),
                     "description": c.get("description"),
                     "owner": c.get("owner"),
-                    "image_url": image_url,
+                    "image_url": c.get("icon_url"),
                     "time_added": c.get("added_timestamp"),
                 }
             )
 
         return render_template("main.html", entries=entries)
-
-    @flask_app.route("/media/entry-icon", methods=["GET"])
-    def entry_icon():
-        entry_id = request.args.get("entry_id")
-
-        filename = f"{entry_id}.img"
-
-        file_path = os.path.join(ICON_FOLDER, filename)
-
-        if not os.path.exists(file_path):
-            return "File not found", 404
-
-        # Get last modified time of the file
-        last_modified = datetime.fromtimestamp(os.path.getmtime(file_path))
-
-        # Compare with client's If-Modified-Since header
-        if_modified_since = request.headers.get("If-Modified-Since")
-        if if_modified_since:
-            try:
-                since_time = datetime.strptime(
-                    if_modified_since, "%a, %d %b %Y %H:%M:%S GMT"
-                )
-                if since_time >= last_modified:
-                    return "", 304
-            except ValueError:
-                pass  # if header is malformed, ignore it
-
-        # TODO Fix the mimetype
-        response = make_response(send_file(file_path, mimetype="image/jpeg"))
-        response.headers["Last-Modified"] = http_date(os.path.getmtime(file_path))
-        response.headers["Cache-Control"] = "public, max-age=86400"
-
-        return response
 
     @flask_app.route("/new_entry")
     def new_entry():
